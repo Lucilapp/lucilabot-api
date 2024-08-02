24fc7bed5f2ea3c13ef2e86a761563077bc4106f
Android Studio <devandroid@ort>
2024-08-02 12:15:46 -0300

    ANDAAAAAAAA

--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -11,8 +11,15 @@ whatsappClient.on('ready', () => console.log('Client Ready'))
 whatsappClient.on("message", async(msg) =>{
     try {
         if(msg.from != 'status@broadcast'){
-            const contact = await msg.getContact()
-            console.log(contact)
+            const contact = await msg.getContact();
+            const chat = await msg.getChat();
+            const chatid = chat.lastMessage.id._serialized;
+            if(msg.body === "hola"){
+                msg.reply("hola, soy lucila, mucho gusto!")
+            }
+            if(msg.body === "soy yuco"){
+                msg.reply("sos un boludo")
+            }
         }
     } catch (error) {
         console.log(error)
